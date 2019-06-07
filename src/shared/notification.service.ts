1741650b9483c0f510373646bcddabe0368a770a
Degor <x.sotomayor@alumnos.upm.es>
2019-06-07 12:15:16 +0200

    Add new notification service #18

---
+++ src/shared/notification.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from '../core/http.service';
+import {ApiEndpoint} from './api-endpoint.model';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {ToastController} from '@ionic/angular';
+
+@Injectable()
+export class NotificationService {
+
+    eventSource: EventSource;
+    topicToSubscribeURL: URL;
+    topicSubject: Subject<string> = new BehaviorSubject(undefined);
+    dataReceive;
+
+    constructor(private http: HttpService, private toastController: ToastController) {
+        this.topicToSubscribeURL = new URL(ApiEndpoint.HUB);
+        this.topicToSubscribeURL.searchParams.append('topic', ApiEndpoint.TOPIC_ASSOCIATION_PETS);
+    }
+
+    subscribeThisTopic() {
+        const url = this.topicToSubscribeURL;
+        this.eventSource = new EventSource('' + url);
+        console.log('SUBSCRIBED');
+        this.eventSource.onmessage = result => {
+            this.dataReceive = result.data;
+            this.topicSubject.next(this.dataReceive);
+        };
+        this.eventSource.onerror = e => this.topicSubject.error(e);
+    }
+
+    topicObservable() {
+        return this.topicSubject.asObservable();
+    }
+}
